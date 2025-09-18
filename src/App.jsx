c1eb642917d2b159a044037e0fbc5340c2906146
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-18 21:14:43 +0800

    head

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className="relative z-10 container mx0-auto  px-4 py-8 m-h-screen">
+      <div className='absolute inset-0 bg-cover bg-center bg-no-repeat'>
+        <img src="" alt="" />
+      </div>
+      <div className="max-w-7xl mx-auto">
+        <div className="text-center mb-12 ">
+          <div className="mb-8">
+            <h1 className="text-5xl md:text-7xl font-bold text-white mb-4 drop-shadow-2xl tracking-tight">
+              Rain
+              <span className="bg-gradient-to-r from-blue-400 to-purple-400 bg-clip-text text-transparent">
+                Check
+              </span>
+            </h1>
+            <p className="text-white/80 text-lg md:text-xl mb-8 max-w-2xl mx-auto loading-relaxed">
+              RainCheck provides real-time weather forecasts with accurate
+              updates, helping you stay prepared for any condition throughout
+              the day
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
