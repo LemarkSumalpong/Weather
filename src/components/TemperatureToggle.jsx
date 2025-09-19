93763ee6c1e2a9e034f243a2672f6e8315bacd7f
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-19 21:50:22 +0800

    temperature ui

--- src/components/TemperatureToggle.jsx
+++ src/components/TemperatureToggle.jsx
@@ -4,7 +4,14 @@ function TemperatureToggle() {
       className="bg-white/10 backdrop-blur-xl border border-white/20 rounded-2xl
     p-1 shadow-lg"
     >
-      <div className="flex items-center"></div>
+      <div className="flex items-center">
+        <button className={`px-4 py-2 rounded-xl text-sm font-semibold transition-all duration-300`}>
+            °C
+        </button>
+         <button className={`px-4 py-2 rounded-xl text-sm font-semibold transition-all duration-300`}>
+            °F
+        </button>
+      </div>
     </div>
   );
 }
