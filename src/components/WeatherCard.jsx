57f288a88a43526b66458db46f8aabab5784a698
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-20 20:43:54 +0800

    weather

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -21,6 +21,23 @@ function WeatherCard() {
           <div className="text-white/50 texm-sx"></div>
         </div>
       </div>
+      <div className="flex items-center justify-between b-10">
+        <div className="flex-1">
+          <div className="text-7xl font-bold text-white mb-3 tracking-tight">
+            Main Temp
+          </div>
+          <div className='text-white/90 text-xl capitalize mb-2 font-semibold'>
+            Weather Description
+          </div>
+          <div className='flex items-center space-x-4 text-white/40 text-sm '>
+          <span>Max Temp</span>
+          <span>Min Temp</span>
+          </div>
+        </div>
+        <div className='textwhite/90 tansform hover:scale-110 transition-transform duration-300'>
+        
+        </div>
+      </div>
     </div>
   );
 }
