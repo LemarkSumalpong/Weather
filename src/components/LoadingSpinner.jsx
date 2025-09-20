1da30e30981ebeef5804c6e36971449f1d60cb77
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-20 20:00:17 +0800

    spinner

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -7,6 +7,10 @@ function LoadingSpinner() {
             border-t-white/20 shadow-lg"
         ></div>
         <div className="absolute inset-2  w-12 h-12 border-blue-200/30 rounded-full animate-spin border-t-blue-200/80 animation-delay-150"></div>
+
+        <div className="absolute inset-0 flex items-center justify-center">
+          <div className="w-3 h-3 bg-white/50 rounded-full animate-pulse"></div>
+        </div>
       </div>
     </div>
   );
