0aef966658fac2c2ead21476604ee809719f78ae
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-21 21:14:29 +0800

    search  function

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,6 @@
 import { MapPin, Search, X } from 'lucide-react';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { searchCities } from '../services/WeatherAPI';
 
 function SearchBar({ onSearch, onlocationSearch, loading }) {
   const [query, setQuery] = useState('');
@@ -7,30 +8,65 @@ function SearchBar({ onSearch, onlocationSearch, loading }) {
   const [showSuggestion, setShowSuggestions] = useState(false);
   const [searchLoading, setSearchLoading] = useState(false);
 
+  const searchRef = useRef();
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (searchRef.current && !searchRef.current.contains(event.target)) {
+        setShowSuggestions(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
+
+  useEffect(() => {
+    const searchTimeout = setTimeout(async () => {
+      if (query.lenght > 2) {
+        setSearchLoading(true);
+
+        try {
+          const result = await searchCities(query);
+          setSuggestions(result);
+          setShowSuggestions(true);
+        } catch (err) {
+          console.error('Search Failed:', err);
+        } finally {
+          searchLoading(false);
+        }
+      } else {
+        setSuggestions([]);
+        setShowSuggestions(false);
+      }
+    }, 300);
+
+    return () => clearTimeout(searchTimeout);
+  }, [query]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(query.trim()){
+    if (query.trim()) {
       onSearch(query.trim());
-      setQuery("");
+      setQuery('');
       setShowSuggestions(false);
     }
   };
 
   const clearSearch = () => {
-    setQuery("");
+    setQuery('');
     setSuggestions([]);
     setShowSuggestions(false);
   };
 
   const handleSuggestionsClick = (city) => {
-    const cityName = city.name ? `${city.name}, ${city.state}` : city.name
+    const cityName = city.name ? `${city.name}, ${city.state}` : city.name;
     onSearch(cityName);
-    setQuery("");
+    setQuery('');
     setShowSuggestions(false);
-  }
+  };
 
   return (
-    <div className="relative w-full max-w-2xl">
+    <div className="relative w-full max-w-2xl" ref={searchRef}>
       <form className="relative" onSubmit={handleSubmit}>
         <div className="relative group">
           <Search className="absolute left-4 top-1/2 transform -translate-y-1/2 text-white/40 w-5 h-5 group-focus-within:text-white transition-all" />
@@ -42,12 +78,12 @@ function SearchBar({ onSearch, onlocationSearch, loading }) {
             className="w-full pl-12 pr-24 py-4 bg-white/10 bacldrop-blur-xl border border-white/20 
             rounded-2xl text-white placeholder-white/50 focus:outline-none focus:ring-2 focus:ring-white/30 focus:border-white/40
             transition-all duration-300 hover:bg-white/15 "
-           
           />
           {query && (
             <button
               className="absolute right-14 top-1/2 transform -translate-y-1/2 text-white/40 hover:text-white transition-all
             p-1 rounded-full hover:bg-white/20"
+              onClick={clearSearch}
             >
               <X className="w-4 h-4" />
             </button>
@@ -56,6 +92,8 @@ function SearchBar({ onSearch, onlocationSearch, loading }) {
           <button
             className="absolute right-4 top-1/2 transform -translate-y-1/2 text-white/40 hover:text-white transition-all
             p-1 rounded-full hover:bg-white/20"
+            onClick={onlocationSearch}
+            disabled={loading}
           >
             <MapPin className="w-5 h-5" />
           </button>
@@ -67,21 +105,30 @@ function SearchBar({ onSearch, onlocationSearch, loading }) {
           className="absolute top-full left-0 right-0 mt-3 bg-white/10 backdrop-blur-xl 
           border border-white/20 rounded-2xl shadow-2xl overflow-hidden z-50"
         >
-          <div className="p-6 text-center text-white/70">
-            <div className="animate-spin rounded-full h-6 w-6 border-2 border-white/30 border-t-white mx-auto"></div>
-            <p className="mt-4">Search Cities...</p>
-          </div>
-          <button
-            className="w-full px-6 py-4 text-left hover:bg-white/20 transition-all duration-200 flex items-center 
-            justify-between group border-b border-white/10"
-          >
-            <div className="font-medium text-white group-hover:text-white/90">
-              City Name
-              <span>,City State</span>
+          {searchLoading ? (
+            <div className="p-6 text-center text-white/70">
+              <div className="animate-spin rounded-full h-6 w-6 border-2 border-white/30 border-t-white mx-auto"></div>
+              <p className="mt-4">Search Cities...</p>
             </div>
-            <div className="text-sm text-white/50">Country</div>
-            <Search className="w-4 h-4 text-white/40 group-hover:text-white/60" />
-          </button>
+          ) : (
+            suggestion.map((city, index) => {
+              return (
+                <button
+                  className="w-full px-6 py-4 text-left hover:bg-white/20 transition-all duration-200 flex items-center 
+            justify-between group border-b border-white/10"
+                  key={`${city.name}-${city.country}-${index}`}
+                  onClick={handleSuggestionsClick(city)}
+                >
+                  <div className="font-medium text-white group-hover:text-white/90">
+                    {city.name}
+                    {city.state && <span>,{city.state}</span>}
+                  </div>
+                  <div className="text-sm text-white/50">{city.country}</div>
+                  <Search className="w-4 h-4 text-white/40 group-hover:text-white/60" />
+                </button>
+              );
+            })
+          )}
         </div>
       )}
     </div>
