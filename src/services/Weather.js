91863bd221d3b1e68aaa7f71ce972119e3d5f4a2
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-21 18:59:09 +0800

    api call current city, forecast, coordiation, and search

--- src/services/Weather.js
+++ src/services/Weather.js
@@ -5,8 +5,8 @@ const GEO_URL = "https://api.openweathermap.org/geo/1.0"
 
  export const getCurrentWeather = async (city) => {
     try {
-        const response = await fetch(`${BASE_URL}/weather=${city}&appid=${API_KEY}&
-        units=metric`)
+        const response = await fetch(`${BASE_URL}/weather?q=${city}&appid=${API_KEY}&
+        units=metric`);
 
         if (!response.ok){
             if(response.status === 404){
@@ -37,8 +37,8 @@ const GEO_URL = "https://api.openweathermap.org/geo/1.0"
 
   export const getCurrentWeatherByCoords = async (lat, lon) => {
     try {
-        const response = await fetch(`${BASE_URL}/weather=${lat}&lon=${lon}&appid=${API_KEY}&
-        units=metric`)
+        const response = await fetch(`${BASE_URL}/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&
+        units=metric`);
 
         if (!response.ok){
             if(response.status === 404){
@@ -63,3 +63,58 @@ const GEO_URL = "https://api.openweathermap.org/geo/1.0"
         throw error;
     }
  };
+
+  export const getCurrentWeatherForecast = async (city) => {
+    try {
+        const response = await fetch(`${BASE_URL}/weather?q=${city}&appid=${API_KEY}&
+        units=metric`);
+
+        if (!response.ok){
+            if(response.status === 404){
+                throw new Error(`city ${city} not found, please try again.`);
+
+            }else if(response.status === 401){
+                throw new Error(`Invalid API key, please check OpenWeatherMap API configuration.`);
+            }
+        }else{
+            throw new Error("Weather seruice is temporary unavailable, please try again.");
+        }
+
+        return  await response.json();
+    } catch(error) {
+        if(error instanceof TypeError && error.message.includes("fetch")){
+            throw new Error("Network error, please check your internet connection");
+        }
+        throw error;
+    }
+ };
+
+ export const searchCities = async (query) => {
+    try {
+        const response = await fetch(`${GEO_URL}/weather?q=${query}&limit=5&appid=${API_KEY}`);
+
+          if (!response.ok){
+            if(response.status === 404){
+                throw new Error(`Invalid API key, please check OpenWeatherMap API configuration.`);
+            }
+        }else{
+            throw new Error("Weather seruice is temporary unavailable, please try again.");
+        }
+
+        const data = await response.json();
+        return data.map((city) => ({
+            name: city.name,
+            lat: city.lat,
+            lon: city.lon,
+            country: city.country,
+            state: city.state || "",
+        }))
+    } catch(error) {
+        if(error instanceof TypeError && error.message.includes("fetch")){
+            throw new Error("Network error, please check your internet connection");
+        }
+        throw error;
+    }
+ };
+
+ 
